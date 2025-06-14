9813007d21d08fe618fe5a764da0544a42237563
NoBody <utk3970@naver.com>
2025-06-14 23:04:12 +0900

    42번째 정답에 근접해감

---
+++ src/app/42/page.tsx
@@ -0,0 +1,7 @@
+export default function FortyTwoPage() {
+    return (
+        <main className="min-h-screen flex items-center justify-center bg-black text-green-400 text-3xl font-mono">
+            당신은 정답을 찾았다
+        </main>
+    );
+}
